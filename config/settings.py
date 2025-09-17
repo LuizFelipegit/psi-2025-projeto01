229456e4b1b44793c96895e664ee6165c85307ca
luizfelipegit <felipesilva253545@gmail.com>
2025-09-17 14:17:23 -0300

    segunda etapa

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -114,13 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from website import views
+from django.conf import settings             
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('website.urls')),
 
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
    
-   
+  
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
